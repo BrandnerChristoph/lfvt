407d81b1c6e810e81d105c454529b42be30f40d9
bn <bn@htlwy.at>
2022-07-15 11:02:44 +0200

    update

--- config/web.php
+++ config/web.php
@@ -76,11 +76,14 @@ $config = [
         'urlManager' => [
             
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
+            'enableStrictParsing' => false,
             'rules' => [
+                /*
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                */
             ],
             //'enablePrettyUrl' => false,
         ],
