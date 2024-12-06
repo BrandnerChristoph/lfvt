4113c640eaa59ca10bc98b65cf6edb6880c46425
Brandner Christoph <bn@htlwy.at>
2024-12-06 18:58:31 +0100

    add preprod environment

---
+++ config/preprod/db.php
@@ -0,0 +1,13 @@
+<?php
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=mysql;dbname=lfv_preprod',
+        'username' => 'usrLfv',
+        'password' => 'T]QaG5WqJZ*hV2Cl',
+        'charset' => 'utf8',
+    
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
