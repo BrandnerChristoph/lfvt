067388aa7c0ea2601a3cfacb47dbfc2ae1ea4bcf
Brandner Christoph <bn@htlwy.at>
2026-05-27 15:51:19 +0200

    reset home-url in index file

--- web/index-preprod.php
+++ web/index-preprod.php
@@ -9,4 +9,22 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web_preprod.php';
 
+
+$app = new yii\web\Application($config);
+
+// detect previous page
+$referrer = Yii::$app->request->referrer;
+
+if ($referrer && strpos($referrer, 'preprod') !== false) {
+    Yii::$app->homeUrl = '/index-preprod.php';
+} else {
+    Yii::$app->homeUrl = '/index.php';
+}
+
+$app->run();
+
+/*
+
+
 (new yii\web\Application($config))->run();
+*/
